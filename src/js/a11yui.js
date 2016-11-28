7caf70c9f4bfc546a34d2d7d9f3db3be20b6977d
William Miner <wgminer@gmail.com>
2016-11-27 23:30:21 -0500

    Working on injecting html and js into page

---
+++ src/js/a11yui.js
@@ -0,0 +1,28 @@
+$(function () {
+    'use strict';
+
+    $('[data-example]').each(function () {
+
+        var $this = $(this);
+        var type = $this.attr('data-example');
+
+        function inject (code) {
+            $('#' + type).append(code);
+        }
+
+        if (type === 'html') {
+            inject($this);
+        }   
+
+        if (type === 'js') {
+
+            $.get($this[0].src, function (js) {
+                console.log(js);
+                inject(js);
+            });
+
+        }
+
+    });
+
+});
